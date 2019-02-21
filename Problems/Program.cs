5b0dc527c703c4a5dd9cdec1cfcf942a3ce85fc0
Lubabalo Buwa <G17B3155@ict.ru.ac.za>
2019-02-21 15:34:29 +0200

    Added the modified Program.cs

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -2,8 +2,23 @@
 
 namespace Problems
 {
-    class Program
+    public class Program
     {
+        public string Prefix(string input)
+        {
+            string output="";
+            string[] words = input.Split();
+            int lngth = 0;
+            int NumberOfWords = words.Length;
+            foreach(char c in input )
+            {
+                lngth++;
+            }
+            output = $"{lngth},{NumberOfWords}:{input}";
+
+            return output;
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
